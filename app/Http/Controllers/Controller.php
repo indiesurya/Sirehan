587038d34f4d13876647483efadbc934bf64ad4b
indiesurya <putuindie11@gmail.com>
2022-05-07 00:58:09 +0800

    change client

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -24,7 +24,7 @@ class Controller extends BaseController
         RdfNamespace::set('owl', 'http://www.w3.org/2002/07/owl#');
         RdfNamespace::set('handphone', 'http://www.semanticweb.org/indiesurya/OntologiHandphone#');
 
-        $this->sparql = new Client('http://lokapaladigital.com:3030/handphone/query');
+        $this->sparql = new Client('http://localhost:3030/Sirehan/query');
     }
     
     public function parseData($str)
